f449a4fc2f1bb3423ce262c217d9278d29120239
learning-dev <ayazm.140895@gmail.com>
2019-12-30 18:48:59 +0530

    show all the cards in each list

--- makeTrello.js
+++ makeTrello.js
@@ -4,42 +4,42 @@ import { storedApiKey, storedTokenSecret } from './keys.js';
 const apiKey = storedApiKey;
 const tokenSecret = storedTokenSecret;
 
-const getCardsUrl = `https://api.trello.com/1/boards/5dce45086b307e7119c6b620/cards?key=${apiKey}&token=${tokenSecret}`;
+const getListsFromBoard = `https://api.trello.com/1/boards/5dce45086b307e7119c6b620/lists?cards=all&key=${apiKey}&token=${tokenSecret}`;
 
-let listOfCards;
+let listCards;
+
 
 function refreshDom() {
   const mainDiv = document.getElementById('main-div');
-  listOfCards.forEach((entry) => {
-    console.log('name', entry['name']);
-    console.log('checkitem', entry['badges']['checkItems']);
-    console.log('checkedItems', entry['badges']['checkItemsChecked']);
-    const nameDiv = document.createElement('div');
-    const checkDiv = document.createElement('div');
-    nameDiv.appendChild(document.createTextNode(entry['name']));
-    checkDiv.appendChild(document.createTextNode(`${entry['badges']['checkItemsChecked']  }/${  entry['badges']['checkItems']}`));
-    const cardDiv = document.createElement('div');
-    cardDiv.setAttribute('class', 'cardDiv');
-    cardDiv.appendChild(nameDiv);
-    cardDiv.appendChild(checkDiv);
-    mainDiv.appendChild(cardDiv);
-
+  listCards.forEach((list) => {
+    const listDiv = document.createElement('div');
+    listDiv.setAttribute('class', 'list-div');
+    list['cards'].forEach((entry) => {
+      const nameDiv = document.createElement('div');
+      const checkDiv = document.createElement('div');
+      nameDiv.appendChild(document.createTextNode(entry.name));
+      checkDiv.appendChild(document.createTextNode(`${entry.badges.checkItemsChecked}/${entry.badges.checkItems}`));
+      const cardDiv = document.createElement('div');
+      cardDiv.setAttribute('class', 'cardDiv');
+      cardDiv.appendChild(nameDiv);
+      cardDiv.appendChild(checkDiv);
+      listDiv.appendChild(cardDiv);
+    });
+    mainDiv.appendChild(listDiv);
+    mainDiv.setAttribute('class', 'container-div');
   });
 }
 
-async function getRequest() {
-  const response = await fetch(getCardsUrl);
+async function getListsAndCards() {
+  const response = await fetch(getListsFromBoard);
   if (response.ok) {
-    console.log('Sucess');
-    listOfCards = await response.json();
+    listCards = await response.json();
+    console.log('list', listCards);
     refreshDom();
-    console.log(listOfCards);
   } else {
     throw new Error('Request Failed!');
   }
 }
 
-getRequest();
 
-
-
+getListsAndCards();
