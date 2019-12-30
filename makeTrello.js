4bdbd627ca27cb9633ca348903f1951a65af3e17
learning-dev <ayazm.140895@gmail.com>
2019-12-30 10:42:40 +0530

    remove api keys from file

--- makeTrello.js
+++ makeTrello.js
@@ -1,17 +1,23 @@
+import { storedApiKey, storedTokenSecret } from './keys.js';
 
-const apiKey = '6c06accb896612f1d725f69c4d447b41';
-const tokenSecret = '8d759400d3be866114d191e053beda422a09bca00058fdc263cf580d1f5b4398';
-console.log('api', apiKey);
+
+const apiKey = storedApiKey;
+const tokenSecret = storedTokenSecret;
 
 const getCardsUrl = `https://api.trello.com/1/boards/5dce45086b307e7119c6b620/cards?key=${apiKey}&token=${tokenSecret}`;
 
 let listOfCards;
+
+function refreshDom() {
+
+}
 
 async function getRequest() {
   const response = await fetch(getCardsUrl);
   if (response.ok) {
     console.log('Sucess');
     listOfCards = await response.json();
+    refreshDom();
     console.log(listOfCards);
   } else {
     throw new Error('Request Failed!');
@@ -19,3 +25,6 @@ async function getRequest() {
 }
 
 getRequest();
+
+
+
