ea81ce63260950d123668478fc006d4e629dcddf
learning-dev <ayazm.140895@gmail.com>
2019-12-31 11:11:23 +0530

    add 'Add a new card field and button '

--- makeTrello.js
+++ makeTrello.js
@@ -15,21 +15,36 @@ function refreshDom() {
     const listDiv = document.createElement('div');
     const ptag = document.createElement('p');
     const listname = document.createTextNode(list['name']);
+
     ptag.appendChild(listname);
     ptag.setAttribute('class', 'list-name-text');
     listDiv.appendChild(ptag);
     listDiv.setAttribute('class', 'list-div');
+
     list['cards'].forEach((entry) => {
       const nameDiv = document.createElement('div');
       const checkDiv = document.createElement('div');
       nameDiv.appendChild(document.createTextNode(entry.name));
       checkDiv.appendChild(document.createTextNode(`${entry.badges.checkItemsChecked}/${entry.badges.checkItems}`));
       const cardDiv = document.createElement('div');
-      cardDiv.setAttribute('class', 'cardDiv');
+      cardDiv.setAttribute('class', 'card-div');
       cardDiv.appendChild(nameDiv);
       cardDiv.appendChild(checkDiv);
       listDiv.appendChild(cardDiv);
     });
+
+    // edit add new card field
+    const FieldDiv = document.createElement('div');
+    const textField = document.createElement('input');
+    const saveBtn = document.createElement('input');
+    saveBtn.setAttribute('type', 'submit');
+    textField.setAttribute('type', 'text');
+    textField.setAttribute('placeholder', 'New Card name');
+    FieldDiv.appendChild(textField);
+    FieldDiv.appendChild(saveBtn);
+    FieldDiv.setAttribute('class', 'add-card');
+    FieldDiv.classList.add('card-div');
+    listDiv.appendChild(FieldDiv);
     mainDiv.appendChild(listDiv);
     mainDiv.setAttribute('class', 'container-div');
   });
@@ -39,7 +54,7 @@ async function getListsAndCards() {
   const response = await fetch(getListsFromBoard);
   if (response.ok) {
     listCards = await response.json();
-    console.log('list', listCards);
+
     refreshDom();
   } else {
     throw new Error('Request Failed!');
